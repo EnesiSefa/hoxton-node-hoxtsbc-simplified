9489795dfe2acf5316f3d863149df5655e01e9a5
enesi sefa <enesisefa380@gmail.com>
2022-09-21 13:58:16 +0200

    restarted the project

--- prisma/migrations/20220921105709_initial_setup/migration.sql
+++ prisma/migrations/20220921105709_initial_setup/migration.sql
@@ -2,16 +2,20 @@
 CREATE TABLE "User" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
-    "pin" INTEGER NOT NULL
+    "pin" TEXT NOT NULL
 );
 
 -- CreateTable
 CREATE TABLE "Transaction" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "price" INTEGER NOT NULL,
+    "amount" INTEGER NOT NULL,
+    "currency" TEXT NOT NULL,
     "userId" INTEGER NOT NULL,
     CONSTRAINT "Transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_name_key" ON "User"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Transaction_currency_key" ON "Transaction"("currency");
--- src/server.ts
+++ src/server.ts
@@ -15,92 +15,25 @@ const prisma = new PrismaClient();
 
 const port = 4000;
 
-const SECRET = process.env.SECRET!;
-
-function getToken(id: number) {
-  return jwt.sign({ id: id }, SECRET, {
-    expiresIn: "2 days",
-  });
-}
-
-async function getCurrentUser(token: string) {
-  // check if the token is valid
-  // if it is, return the user this token belongs to
-
-  const decodedData = jwt.verify(token, SECRET);
-  const user = await prisma.user.findUnique({
-    // @ts-ignore
-    where: { id: decodedData.id },
-    include: { transactions: true },
-  });
-  return user;
-}
-
-app.get("/users", async (req, res) => {});
-
 app.post("/sign-up", async (req, res) => {
-  try {
-    const match = await prisma.user.findUnique({
-      where: { name: req.body.name },
-    });
-
-    if (match) {
-      res.status(400).send({ error: "This account already exists." });
-    } else {
-      const user = await prisma.user.create({
-        data: {
-          name: req.body.name,
-          pin: bcrypt.hashSync(req.body.pin),
-        },
-        include: { transactions: true },
-      });
-
-      res.send({ user: user, token: getToken(user.id) });
-    }
-  } catch (error) {
-    // @ts-ignore
-    res.status(400).send({ error: error.message });
-  }
+  const user = await prisma.user.create({
+    data: { name: req.body.name, pin: bcrypt.hashSync(req.body.pin) },
+  });
+  res.send(user);
 });
 
 app.post("/sign-in", async (req, res) => {
-  // 1. check if a user with this name actually exists
-  const user = await prisma.user.findUnique({
-    where: { name: req.body.name },
-    include: { transactions: true },
-  });
+  const user = await prisma.user.findUnique({ where: { name: req.body.name } });
   if (user && bcrypt.compareSync(req.body.pin, user.pin)) {
-    // 2. check if the pin matches
-    res.send({ user: user, token: getToken(user.id) });
+    res.send(user);
   } else {
-    res.status(400).send({ error: "Invalid name/pin combination." });
+    res.status(400).send({ error: "name/pin not correct" });
   }
 });
 
-app.get("/validate", async (req, res) => {
-  try {
-    if (req.headers.authorization) {
-      const user = await getCurrentUser(req.headers.authorization);
-      // @ts-ignore
-      res.send({ user, token: getToken(user.id) });
-    }
-  } catch (error) {
-    // @ts-ignore
-    res.status(400).send({ error: error.message });
-  }
-});
+app.get("/validate", async (req, res) => {});
 
-app.get("/transactions", async (req, res) => {
-  try {
-    // @ts-ignore
-    const user = await getCurrentUser(req.headers.authorization);
-
-    res.send(user?.transactions);
-  } catch (error) {
-    // @ts-ignore
-    res.status(400).send({ error: error.message });
-  }
-});
+app.get("/transactions", async (req, res) => {});
 
 app.listen(port, () => {
   console.log(`App running: http://localhost:${port}`);
